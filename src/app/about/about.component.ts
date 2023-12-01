36777265a861b81627382e3c0342ec48778c0a89
DavideBernardini <davidebernardini09@gmail.com>
2023-12-01 13:30:53 +0100

    stuff

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable, noop } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,6 +11,22 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const http$ = new Observable(observer => {
+      fetch('/api/courses')
+        .then(response => response.json())
+        .then(body => {
+          observer.next(body)
+          observer.complete()
+        })
+        .catch(err => observer.error(err))
+    })
+
+    http$.subscribe(
+      courses => console.log(courses),
+      noop,
+      () => console.log('complete')
+    )
+
   }
 
 }
